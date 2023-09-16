8f8e6b29c1f15c1032732d2b89a39c1bb200d5d2
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-09-16 18:50:08 +0700

    add model TableTypeModel

---
+++ RSUP_Master_Item.Logics/Model/TableTypeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSUP_Master_Item.Logics.Model
+{
+    public class TableTypeModel
+    {
+        public int id { get; set; }
+        public string unit_name { get; set; }
+    }
+}
