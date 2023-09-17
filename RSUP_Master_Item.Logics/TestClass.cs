0ab43a8e20980689a427a790bf736494d558b7e5
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-09-17 17:34:19 +0700

    success execute stored procedure with table-valued parameter

--- RSUP_Master_Item.Logics/TestClass.cs
+++ RSUP_Master_Item.Logics/TestClass.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +12,42 @@ namespace RSUP_Master_Item.Logics
     {
         public void CreateMasterItemFromQuery(List<string> unit_name)
         {
-            var fullQuery = "";
+            string connectionString = "Data Source=10.0.0.50;Initial Catalog=Sambu_Staging_RSUP;User ID = sa; Password=pass@word1";
+            var conn = new SqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                #region add the table
+                DataTable dataTable = new DataTable();
+                dataTable.Columns.Add("id", typeof(int));
+                dataTable.Columns.Add("unit_name", typeof(string));
+                #endregion
+                #region add data to the created table
+                for(int i = 0; i < unit_name.Count; i++)
+                {
+                    dataTable.Rows.Add((i + 1), unit_name[i]);
+                }
+                #endregion
 
+                var command = new SqlCommand("dbo.usp_Create_Master_item_Data", conn);
+                command.CommandType = CommandType.StoredProcedure;
+                SqlParameter tableParam = command.Parameters.AddWithValue("@InputTable", dataTable);
+                tableParam.SqlDbType = SqlDbType.Structured;
+                tableParam.TypeName = "dbo.MyTableType";
 
-            Console.WriteLine(fullQuery);
+                SqlDataReader reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    Console.WriteLine("id\tunit_name");
+                    Console.WriteLine(reader["id"] + "\t" + reader["unit_name"]);
+                }
+                reader.Close();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
--- RSUP_Master_Item/Program.cs
+++ RSUP_Master_Item/Program.cs
@@ -1,7 +1,12 @@
-﻿public class Program
+﻿using RSUP_Master_Item.Logics;
+
+public class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        TestClass testClass = new TestClass();
+        List<string> units = new List<string>{ "Butir", "Kg"};
+        testClass.CreateMasterItemFromQuery(units);
     }
 }
